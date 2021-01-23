95a38e0f109255c1564d90b5034866554bcdc8fa
DDR15 <65582291+RavenRivaldo@users.noreply.github.com>
2021-01-23 12:13:31 +0700

    gbook.php
    test

---
+++ gbook.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Buku Tamu</title>
+</head>
+<body>
+	<h3>Buku Tamu</h3>
+	<form method="POST" action="gbproses.php">
+	<table border="0" width="100%" cellspacing="1" cellpadding="0">
+		<tr>
+			<td>Nama</td>
+			<td><input type="text" name="nama" size="20"></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td><input type="text" name="nama" size="20"></td>
+		</tr>
+		<tr>
+			<td>Komentar</td>
+			<td><textarea rows="5" name="komentar" cols="50"></textarea></td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type="submit" value="Submit" name="submit"><input type="reset" value="Reset" name="reset"></td>
+		</tr>
+	</table>
+	</form>	
+</body>
+</html>
